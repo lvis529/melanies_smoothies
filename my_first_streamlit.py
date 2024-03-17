cbb3e6b43054afdd5fd7e01db0946b6b5cafd023
lvis529 <62891586+lvis529@users.noreply.github.com>
2024-03-17 22:03:04 +0530

    Update my_first_streamlit.py

--- my_first_streamlit.py
+++ my_first_streamlit.py
@@ -36,8 +36,10 @@ if ingredients_list:
     ingredient_str=''
     for fruits in ingredients_list:
         ingredient_str+=fruits+' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruits, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruits,' is ', search_on, '.')
         st.subheader(fruits+ ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruits)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+search_on)
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
 
